3221f7582a8b0eee7b0ccfcde91fd6150a8f0aa7
Rational-Curiosity <hecenra@gmail.com>
2023-03-23 00:19:48 +0100

    fish auto update title

--- fish/config.fish
+++ fish/config.fish
@@ -60,6 +60,25 @@ abbr -a ..11 'cd ../../../../../../../../../../..'
 
 bind \ez 'commandline -r fg; commandline -f execute'
 
+function fish_title
+    # emacs' "term" is basically the only term that can't handle it.
+    if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
+        # If we're connected via ssh, we print the hostname.
+        set -l ssh
+        set -q SSH_TTY
+        and set ssh "["(prompt_hostname | string sub -l 10 | string collect)"]"
+        # An override for the current command is passed as the first parameter.
+        # This is used by `fg` to show the true process name, among others.
+        if set -q argv[1]
+            echo -- $ssh (prompt_pwd -d 1 -D 1)❯ (string sub -l 20 -- $argv[1])
+        else
+            # Don't print "fish" because it's redundant
+            set -l command (status current-command)
+            echo -- $ssh (prompt_pwd -d 1 -D 1)❯ (string sub -l 20 -- $command)
+        end
+    end
+end
+
 function zoxide-add-for -a times folder
     for i in $(seq $times); zoxide add $folder; end
 end
