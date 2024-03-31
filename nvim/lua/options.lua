6a2757befc57bd13549bdad03a961d92a515f21a
Rational-Curiosity <hecenra@gmail.com>
2024-03-31 14:57:15 +0200

    refactor: relocate key bindings

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -107,20 +107,6 @@ vim.cmd([[
     autocmd TermOpen term://* setlocal scrollback=100000 nospell nonumber norelativenumber|startinsert
   augroup end
 
-  " SNIPPETS
-  " press <Tab> to expand or jump in a snippet. These can also be mapped separately
-  " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-  imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-  " -1 for jumping backwards.
-  inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-  snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-  snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
-
-  " For changing choices in choiceNodes (not strictly necessary for a basic setup).
-  imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-  smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-
   " MATCHPAREN
   let g:matchparen_timeout = 150
   let g:matchparen_insert_timeout = 50
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -545,6 +545,12 @@ M = {
         treesitter = "TS",
       }
 
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+      local luasnip = require("luasnip")
+
       local cmp = require("cmp")
       cmp.setup({
         performance = {
@@ -572,17 +578,34 @@ M = {
             return vim_item
           end
         },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-a>"] = cmp.mapping.abort(),
           ["<C-e>"] = cmp.mapping.close(),
-          ["<TAB>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- that way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
