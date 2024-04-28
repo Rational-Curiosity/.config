8318b2f188642aa44b7de461c5a86ddc2f123f63
Rational-Curiosity <hecenra@gmail.com>
2024-04-28 18:20:35 +0200

    fix: nvim-notify loaded by noice

--- nvim/lua/plugins/visual.lua
+++ nvim/lua/plugins/visual.lua
@@ -1,7 +1,7 @@
 return {
   {
     "rcarriga/nvim-notify",
-    event = "VeryLazy",
+    -- event = "VeryLazy",
     config = function()
       local util = require("notify.stages.util")
       local notify = require("notify")
