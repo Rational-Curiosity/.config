f5fdbad1570f828f82e243ee83c0d3d74cb281d1
Rational-Curiosity <hecenra@gmail.com>
2024-05-15 21:43:07 +0200

    feat: added oil.nvim

--- nvim/lua/plugins/visual.lua
+++ nvim/lua/plugins/visual.lua
@@ -148,4 +148,18 @@ return {
         require("icon-picker").setup({ disable_legacy_commands = true })
     end,
   },
+  {
+    "stevearc/oil.nvim",
+    cmd = { "Oil" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      default_file_explorer = false,
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+      },
+    },
+  },
 }
