149135b8ad7c139f3817c922a038c4ff96883da9
Rational-Curiosity <hecenra@gmail.com>
2024-05-17 10:55:29 +0200

    feat: comment built in support in neovim 0.10.0

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -4,14 +4,4 @@ return {
     keys = { { "<leader>B", ":VBox<cr>", mode = "x", desc = "VBox" } },
     cmd = { "VBox" },
   },
-  {
-    "numToStr/Comment.nvim",
-    keys = {
-      { "gc", mode = { "n", "x" }, desc = "Comment" },
-      { "gb", mode = { "n", "x" }, desc = "Comment block" },
-    },
-    config = function()
-      require("Comment").setup()
-    end,
-  },
 }
